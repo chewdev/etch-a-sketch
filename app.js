f5c9821b863ef27f67363ce33f70227d91511f36
Chris Ewald <itsbewald@gmail.com>
2022-01-17 21:40:50 -0800

    Split UI controls, show current selected option

--- app.js
+++ app.js
@@ -4,40 +4,131 @@ colorPickerProxyEl.addEventListener("click", () => {
   colorPickerEl.focus();
 });
 
-const colorMessageEl = document.querySelector(".color-message");
+// const colorMessageEl = document.querySelector(".color-message");
 const recentColorsEl = document.querySelector(".recent-colors");
 
+const fillBtnEl = document.querySelector(".fill");
+fillBtnEl.addEventListener("click", () => {
+  userColorPicker.toggleUseFill();
+});
+
+const drawBtnEl = document.querySelector(".draw");
+drawBtnEl.addEventListener("click", () => {
+  userColorPicker.setUseDraw();
+});
+
 function colorPicker() {
-  this.colorMessage = "";
+  //   this.colorMessage = "";
   this.currentColor = "#000000";
   this.previousColors = [];
+  this.fillColor = "#777777";
+  this.columns = 16;
+  this.rows = 16;
+  this.useDraw = true;
+  this.useFill = false;
+  this.useEraser = false;
   this.useRandomColor = false;
   this.useDarkenColor = false;
   this.useLightenColor = false;
-  this.toggleUseRandomColor = function () {
-    this.useRandomColor = !this.useRandomColor;
-    if (
-      this.currentColor !==
-      (this.previousColors &&
-        this.previousColors[this.previousColors.length - 1])
-    ) {
-      this.previousColors.push(this.currentColor);
-      this.showRecentColors();
+  this.setUseDraw = function (shouldUpdate = true) {
+    this.useDraw = true;
+    drawBtnEl.classList.add("selected");
+    if (shouldUpdate) {
+      this.updateOtherControls("draw");
     }
-    this.useDarkenColor = this.useLightenColor = false;
   };
-  this.toggleUseDarkenColor = function () {
+  this.unsetUseDraw = function () {
+    this.useDraw = false;
+    drawBtnEl.classList.remove("selected");
+  };
+  this.toggleUseFill = function (shouldUpdate = true) {
+    this.useFill = !this.useFill;
+    if (this.useFill) {
+      fillBtnEl.classList.add("selected");
+      this.updateOtherControls("fill");
+    } else {
+      fillBtnEl.classList.remove("selected");
+      this.setUseDraw(false);
+    }
+  };
+  this.toggleUseEraser = function (shouldUpdate = true) {
+    this.useEraser = !this.useEraser;
+    if (this.useEraser) {
+      this.setColor("#ffffff");
+      eraserEl.classList.add("selected");
+      if (shouldUpdate) {
+        this.updateOtherControls("eraser");
+      }
+    } else {
+      eraserEl.classList.remove("selected");
+      if (this.previousColors.length) {
+        this.previous();
+      }
+      this.setUseDraw(false);
+    }
+  };
+  this.toggleUseRandomColor = function (shouldUpdate = true) {
+    this.useRandomColor = !this.useRandomColor;
+    if (this.useRandomColor) {
+      randomBtnEl.classList.add("selected");
+      if (
+        this.currentColor !==
+        (this.previousColors &&
+          this.previousColors[this.previousColors.length - 1])
+      ) {
+        this.previousColors.push(this.currentColor);
+        this.showRecentColors();
+      }
+      if (shouldUpdate) {
+        this.updateOtherControls("random");
+      }
+    } else {
+      randomBtnEl.classList.remove("selected");
+      this.setUseDraw(false);
+    }
+  };
+  this.toggleUseDarkenColor = function (shouldUpdate = true) {
     this.useDarkenColor = !this.useDarkenColor;
     if (this.useDarkenColor) {
-      this.useRandomColor = this.useLightenColor = false;
+      darkenEl.classList.add("selected");
+      if (shouldUpdate) {
+        this.updateOtherControls("darken");
+      }
+    } else {
+      darkenEl.classList.remove("selected");
+      this.setUseDraw(false);
     }
   };
-  this.toggleUseLightenColor = function () {
+  this.toggleUseLightenColor = function (shouldUpdate = true) {
     this.useLightenColor = !this.useLightenColor;
     if (this.useLightenColor) {
-      this.useRandomColor = this.useDarkenColor = false;
+      lightenEl.classList.add("selected");
+      if (shouldUpdate) {
+        this.updateOtherControls("lighten");
+      }
+    } else {
+      lightenEl.classList.remove("selected");
+      this.setUseDraw(false);
     }
   };
+  this.updateOtherControls = function (selectedBtn) {
+    if (this.useFill && selectedBtn !== "fill") {
+      this.toggleUseFill(false);
+    } else if (this.useEraser && selectedBtn !== "eraser") {
+      this.toggleUseEraser(false);
+    } else if (this.useRandomColor && selectedBtn !== "random") {
+      this.toggleUseRandomColor(false);
+    } else if (this.useDarkenColor && selectedBtn !== "darken") {
+      this.toggleUseDarkenColor(false);
+    } else if (this.useLightenColor && selectedBtn !== "lighten") {
+      this.toggleUseLightenColor(false);
+    }
+
+    if (this.useDraw && selectedBtn !== "draw") {
+      this.unsetUseDraw();
+    }
+  };
+  this.setUseDraw();
   this.getRandomColor = function () {
     let colorStr = "#";
     for (let i = 0; i < 6; i++) {
@@ -56,25 +147,23 @@ function colorPicker() {
     this.showRecentColors();
     this.currentColor = newColor;
     colorPickerProxyEl.style.backgroundColor = newColor;
-    if (colorMessageEl.textContent !== "") {
-      this.colorMessage = "";
-      colorMessageEl.textContent = "";
-    }
-    this.useRandomColor = false;
-    this.useDarkenColor = false;
-    this.useLightenColor = false;
+    // if (colorMessageEl.textContent !== "") {
+    //   this.colorMessage = "";
+    //   colorMessageEl.textContent = "";
+    // }
   };
   this.previous = function () {
     if (this.previousColors.length) {
       this.setColor(this.previousColors.pop(), true);
       colorPickerEl.value = this.currentColor;
-    } else {
-      this.colorMessage =
-        "No previous colors available, please choose a new color.";
-      if (colorMessageEl.textContent !== this.colorMessage) {
-        colorMessageEl.textContent = this.colorMessage;
-      }
     }
+    // else {
+    //   this.colorMessage =
+    //     "No previous colors available, please choose a new color.";
+    //   if (colorMessageEl.textContent !== this.colorMessage) {
+    //     colorMessageEl.textContent = this.colorMessage;
+    //   }
+    // }
   };
   this.showRecentColors = function () {
     if (this.previousColors.length) {
@@ -145,7 +234,7 @@ function setBackground(e) {
 
 const eraserEl = document.querySelector(".eraser");
 eraserEl.addEventListener("click", () => {
-  userColorPicker.setColor("#ffffff");
+  userColorPicker.toggleUseEraser();
 });
 
 function getDarkenHex(hexColor, beginInd, endInd) {
@@ -204,6 +293,10 @@ function getStyle(el, styleProp) {
   return document.defaultView.getComputedStyle(el, null)[styleProp];
 }
 
+function isSameBackgroundColor(el1, el2) {
+  return getStyle(el1, "backgroundColor") === getStyle(el2, "backgroundColor");
+}
+
 // Grid related code
 function createGrid(rows, columns) {
   const container = document.createElement("div");
@@ -217,15 +310,20 @@ function createGrid(rows, columns) {
       let newGridItem = document.createElement("div");
       newGridItem.classList.add(`${i}-${j}`, "grid-item");
       newGridItem.style.width = `${100 / columns}%`;
-      //   newGridItem.addEventListener("click", (e) => {
-      //     setBackground(e);
-      //   });
+      newGridItem.addEventListener("click", (e) => {
+        if (userColorPicker.useFill) {
+          fill(e.target);
+        } else {
+          setBackground(e);
+        }
+      });
       newGridItem.addEventListener("pointerdown", (e) => {
-        // e.stopPropagation();
         newGridItem.releasePointerCapture(e.pointerId); // <- Enables touch sensing using pointerenter for all other elements after touch start
       });
       newGridItem.addEventListener("pointerenter", (e) => {
-        // e.stopPropagation();
+        if (userColorPicker.useFill) {
+          return;
+        }
         setBackground(e);
       });
 
@@ -246,6 +344,7 @@ const createGridInput = document.querySelector("#grid-input");
 createGridInput.addEventListener("change", (e) => {
   let rows, columns;
   rows = columns = e.target.value;
+  userColorPicker.rows = userColorPicker.columns = rows;
   if (document.querySelector(".container")) {
     removeGrid(rows, columns);
   } else {
@@ -263,3 +362,121 @@ const clearEl = document.querySelector(".clear");
 clearEl.addEventListener("click", () => {
   removeGrid(createGridInput.value, createGridInput.value);
 });
+
+function checkConnectedSameColor(
+  currBackgroundColor,
+  checkedEls,
+  row,
+  column,
+  rowDiff,
+  columnDiff
+) {
+  const checkRow = Number(row) + rowDiff;
+  const checkColumn = Number(column) + columnDiff;
+  const checkInd = `${checkRow}-${checkColumn}`;
+  if (checkedEls[checkInd]) {
+    return [];
+  }
+  const checkEl = document.getElementsByClassName(checkInd)[0];
+  checkedEls[checkInd] = true;
+  if (currBackgroundColor === getStyle(checkEl, "backgroundColor")) {
+    return findConnectedSameColor(checkEl, checkedEls);
+  } else {
+    return [];
+  }
+}
+
+function findConnectedSameColor(el, checkedEls = {}) {
+  let connectedEls = [el];
+  let currentElInd;
+  const currBackgroundColor = getStyle(el, "backgroundColor");
+  el.classList.forEach((className) => {
+    if (className.match(/\d+-\d+/)) {
+      currentElInd = className;
+    }
+  });
+  let [row, column] = currentElInd.split("-");
+  checkedEls[`${row}-${column}`] = true;
+  // check left
+  if (column >= 1) {
+    connectedEls = connectedEls.concat(
+      checkConnectedSameColor(
+        currBackgroundColor,
+        checkedEls,
+        row,
+        column,
+        0,
+        -1
+      )
+    );
+  }
+  // check right
+  if (column <= userColorPicker.columns - 2) {
+    connectedEls = connectedEls.concat(
+      checkConnectedSameColor(
+        currBackgroundColor,
+        checkedEls,
+        row,
+        column,
+        0,
+        1
+      )
+    );
+  }
+  //check up
+  if (row >= 1) {
+    connectedEls = connectedEls.concat(
+      checkConnectedSameColor(
+        currBackgroundColor,
+        checkedEls,
+        row,
+        column,
+        -1,
+        0
+      )
+    );
+  }
+  //check down
+  if (row <= userColorPicker.rows - 2) {
+    connectedEls = connectedEls.concat(
+      checkConnectedSameColor(
+        currBackgroundColor,
+        checkedEls,
+        row,
+        column,
+        1,
+        0
+      )
+    );
+  }
+  return connectedEls;
+}
+
+function fill(el) {
+  let fillEls = findConnectedSameColor(el);
+  fillEls.forEach((el) => {
+    el.style.backgroundColor = userColorPicker.currentColor;
+  });
+}
+
+function setSameHeight() {
+  const left = document.querySelector(".colors-container");
+  const topRight = document.querySelector(".grid-input-label");
+  const bottomRight = document.querySelector(".range-container");
+  const leftHeightPx = getStyle(left, "height");
+  const topRightHeightPx = getStyle(topRight, "height");
+  const leftHeight = leftHeightPx.match(/\d+/)[0];
+  const topRightHeight = topRightHeightPx.match(/\d+/)[0];
+  const bottomRightHeight = Number(leftHeight) - Number(topRightHeight);
+  bottomRight.style.height = `${bottomRightHeight - 2}px`;
+}
+
+setSameHeight();
+
+/*
+TODO:
+Make controls into a select so that only one option is selected at a time
+Add Draw option for the default draw
+Add .selected class to whichever option is currently selected
+
+*/
